1fb988db4d634b5e868fb9752e7bc97042bba384
Tasfirul Haque <adef05675@gmail.com>
2025-05-20 20:49:33 +0600

    Fix

--- src/pages/AdminPannelPages/Index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-// This component simply redirects to the PropertiesPage which is now our main page
-const Index = () => {
-  return <Navigate to="/" replace />;
-};
-
-export default Index;
